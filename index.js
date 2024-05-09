ac776c56a1e7f412e693c75b0831ab7cb9fae5f4
PUGALENDHI.S <pugalp702@gmail.com>
2024-05-09 12:48:52 +0530

    updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const js=require("json-server");
 const server=js.create();
 const router=js.router("db.json");
 const mv=js.defaults();
-const port=process.env.PORT||3005;
+const port=process.env.PORT||4000;
 
 server.use(mv);
 server.use(router);
